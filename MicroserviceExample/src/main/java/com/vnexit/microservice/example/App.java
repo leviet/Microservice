62c7f536ae21e674862fc4c65f125a60d1658dcf
Numb <leviet@vnexit.com>
2016-07-22 14:10:13 +0700

    Init project

---
+++ MicroserviceExample/src/main/java/com/vnexit/microservice/example/App.java
@@ -0,0 +1,25 @@
+package com.vnexit.microservice.example;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+/**
+ * Hello world!
+ *
+ */
+@Path("myresource")
+public class App 
+{
+	/**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/plain" media type.
+     *
+     * @return String that will be returned as a text/plain response.
+     */
+    @GET
+    @Path("person")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getIt() {
+        return "Got it!";
+    }
+}
